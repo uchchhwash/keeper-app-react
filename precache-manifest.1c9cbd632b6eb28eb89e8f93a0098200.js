df3412dfcf4b5852b165c228f80e571d3e55cfed
uchchhwash <uchchhwash.git@gmail.com>
2020-06-22 04:53:24 +0600

    Updates

---
+++ precache-manifest.1c9cbd632b6eb28eb89e8f93a0098200.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "494bec6fb4c4bbbc496eb2beed744320",
+    "url": "/keeper-app-react/index.html"
+  },
+  {
+    "revision": "fd92a82cda07a0f96b57",
+    "url": "/keeper-app-react/static/js/2.704f0357.chunk.js"
+  },
+  {
+    "revision": "a38b84e7587697b439b5",
+    "url": "/keeper-app-react/static/js/main.e61ebde3.chunk.js"
+  },
+  {
+    "revision": "bd01b4841d1b47aeb9f7",
+    "url": "/keeper-app-react/static/js/runtime-main.062ef14a.js"
+  }
+]);
