1887ad7f5e6636fea2d60913a1a2410fbbfe0b2e
uchchhwash <uchchhwash.git@gmail.com>
2020-06-22 01:43:28 +0600

    notes state manage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
-      <Footer />
-    </div>
-  );
+    const [noteList, addNotelist] = useState([])
+
+    function insertNote(notes) {
+        addNotelist(noteList => {
+            return [...noteList, notes]
+        })
+    }
+
+    return (
+        <div>
+            <Header />
+            <CreateArea addNote={insertNote} />
+            {noteList.map((note) => <Note key={1} title={note.title} content={note.content} />)}
+          
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
-  return (
-    <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
-      </form>
-    </div>
-  );
+function CreateArea(props) {
+    const [notes, addNote] = useState({
+        title: "",
+        content: ""
+    })
+
+
+    function handleChange(evt) {
+        const { name, value } = evt.target;
+
+        addNote((prevNote) => {
+            return {
+                ...prevNote,
+                [name]: value
+            }
+        })
+
+    }
+    return (
+        <div>
+            <form>
+                <input name="title" placeholder="Title" value={notes.title} onChange={handleChange} />
+                <textarea name="content" placeholder="Take a note..." rows="3" value={notes.content} onChange={handleChange} />
+                <button onClick={(evt) => {
+                    evt.preventDefault();
+                    props.addNote(notes);
+                }} >Add</button>
+            </form>
+        </div>
+    );
 }
 
 export default CreateArea;
+// onClick={(evt) => props.addNote(evt)}
